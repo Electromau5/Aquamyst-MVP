2b3d73d979acc658bd1e3dadf392a2f9fa3030f3
Pritish Sai <Pritish.sai@gmail.com>
2017-02-21 15:40:21 +0530

    changed search feature

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -49,8 +49,8 @@ class Listing < ActiveRecord::Base
 def self.search(search)
   if search
     search_length = search.split.length
-    where([(['name ILIKE ?'] * search_length).join(' AND ')] + search.split.map { |name| "%#{name}%" })
-    where([(['tag ILIKE ?'] * search_length).join(' AND ')] + search.split.map { |name| "%#{tag}%" })
+    where([(['name ILIKE ?'] * search_length).join(' AND ')] + search.split.map { |name| "%#{name}%" }) 
+    where([(['tag ILIKE ?'] * search_length).join(' AND ')] + search.split.map { |tag| "%#{tag}%" })
   else
     find(:all)
   end 
