8b745865ababdd438977a67d224f6747b785333d
Pritish Sai <Pritish.sai@gmail.com>
2017-05-02 23:01:15 +0530

    twitter share button for image

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,6 @@ class ApplicationController < ActionController::Base
 
   def show
     set_meta_tags title: "We are a T-Shirt search engine"
-    set_meta_tags :og => {
-                      :image => @listing.image.url(:medium)
-                      }
   end
 
   
